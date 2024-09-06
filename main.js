61c1e3b7fb0d268f7185409cb6fe2a16dae4b611
Anil Avvaru <anil.avvaru.cool@gmail.com>
2024-09-05 20:52:05 -0400

    web component changes

--- main.js
+++ main.js
@@ -1,3 +1,53 @@
+class MyScript extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        `
+    }
+}
+
+customElements.define('my-script',MyScript);
+
+class MyNavBar extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">          
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Skills</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Certifications</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Clients</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Testimonial</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>`
+    }
+}
+
+customElements.define('my-nav-bar',MyNavBar);
+
 class MyHeader extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
@@ -11,3 +61,15 @@ class MyHeader extends HTMLElement{
 }
 
 customElements.define('my-header',MyHeader);
+
+class MyFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <footer>
+  <p>&copy; All rights reserved Anil Avvaru</p>  
+</footer>`
+    }
+}
+
+customElements.define('my-footer',MyFooter);
+
