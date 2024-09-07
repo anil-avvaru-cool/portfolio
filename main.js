6051f22b605e8cfa576d82bc288f9c44789d9f90
Anil Avvaru <anil.avvaru.cool@gmail.com>
2024-09-07 05:59:07 -0400

    added gulp file

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 class MyScript extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
         `
     }
 }
@@ -55,7 +55,7 @@ class MyHeader extends HTMLElement{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Freelance software engineering</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="css/main.css" rel="stylesheet">
 </head>`
     }
 }
